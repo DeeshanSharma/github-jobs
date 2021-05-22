9e4ca86d5a6b18ac2c50d786d7fc47510402931a
Deeshan Sharma <deeshansharma04@gmail.com>
2021-05-22 23:08:40 +0530

    style: style using material ui also responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,57 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import useFetchJobs from "../useFetchJobs";
 import Job from "./Job";
 import Search from "./Search";
 import Pagination from "./Pagination";
+import Loading from "./Loading";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		marginTop: 20,
+	},
+	msgContainer: {
+		marginTop: 50,
+		textAlign: "center",
+	},
+	jobsContainer: {
+		display: "flex",
+		flexWrap: "wrap",
+		alignItems: "center",
+		justifyContent: "space-between",
+		[theme.breakpoints.down(600)]: {
+			justifyContent: "center",
+		},
+		"& h4": {
+			width: "100%",
+			textAlign: "center",
+		},
+	},
+}));
 
 function Home(props) {
+	const classes = useStyles();
 	const [params, setParams] = useState({});
 	const [page, setPage] = useState(0);
 	const { jobs, loading, error, jobsAvailable, nextPageAvailable } = useFetchJobs(params, page);
 
 	return (
-		<>
+		<div>
 			<Search setParams={setParams} />
 			<Pagination page={page} setPage={setPage} nextPageAvailable={nextPageAvailable} />
-			{loading && <Typography variant="h4">Loading...</Typography>}
-			{error && <Typography variant="h4">Something went wrong..! Please try refreshing</Typography>}
-			{!jobsAvailable
-				? !loading && !error && <Typography variant="h4">No Jobs Available</Typography>
-				: jobs.map((job) => {
-						return <Job key={job.id} job={job} />;
-				  })}
-		</>
+			<div className={classes.msgContainer}>
+				{loading && <Loading />}
+				{error && <Typography variant="h4">Something went wrong..! Please try refreshing</Typography>}
+			</div>
+			<div className={classes.jobsContainer}>
+				{!jobsAvailable
+					? !loading && !error && <Typography variant="h4">No Jobs Available</Typography>
+					: jobs.map((job) => {
+							return <Job key={job.id} job={job} />;
+					  })}
+			</div>
+		</div>
 	);
 }
 
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,22 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Chip, Typography, Button, Collapse, IconButton } from "@material-ui/core";
+import LocationIcon from "@material-ui/icons/LocationOn";
+import WatchIcon from "@material-ui/icons/WatchLater";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Markdown from "markdown-to-jsx";
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		maxWidth: 345,
+		margin: "40px 0",
+		textAlign: "center",
+		wordBreak: "break-word",
+		[theme.breakpoints.down(960)]: {
+			maxWidth: 265,
+		},
+		[theme.breakpoints.down(600)]: {
+			minWidth: 265,
+			maxWidth: "unset",
+		},
+		[theme.breakpoints.between(960, 1150)]: {
+			maxWidth: "calc(100%/3 - 30px)",
+		},
+		[theme.breakpoints.between(600, 960)]: {
+			maxWidth: "calc(100%/2 - 20px)",
+		},
+	},
+	imgContainer: {
+		maxWidth: 345,
+		padding: 10,
+		[theme.breakpoints.down(960)]: {
+			maxWidth: 400,
+		},
+		[theme.breakpoints.down(600)]: {
+			maxWidth: "unset",
+		},
+		[theme.breakpoints.between(600, 1150)]: {
+			maxWidth: "unset",
+		},
+	},
+	media: {
+		width: "100%",
+	},
+	expand: {
+		transform: "rotate(0deg)",
+		marginLeft: "auto",
+		transition: theme.transitions.create("transform", {
+			duration: theme.transitions.duration.shortest,
+		}),
+	},
+	expandOpen: {
+		transform: "rotate(180deg)",
+	},
+	chipLabels: {
+		display: "flex",
+		justifyContent: "center",
+		flexWrap: "wrap",
+		margin: "5px 0",
+		"& > *": {
+			margin: theme.spacing(0.625),
+		},
+	},
+}));
+
 function Jobs(props) {
+	const classes = useStyles();
+	const [expanded, setExpanded] = useState(false);
+
+	function handleExpandClick() {
+		setExpanded(!expanded);
+	}
 	return (
-		<>
-			<h1>{props.job.title}</h1>
-			<span>{new Date(props.job.created_at).toLocaleDateString()}</span>
-			<h3>{props.job.company}</h3>
-			<p>
-				<span>{props.job.type}</span>
-				<span>{props.job.location}</span>
-			</p>
-			<a href={props.job.url} target="_blank" rel="noreferrer">
-				Apply Now
-			</a>
-			<img src={props.job.company_logo} alt="" height="50px" />
-			<Markdown>{props.job.description}</Markdown>
-		</>
+		<Card variant="outlined" className={classes.root}>
+			<CardHeader title={props.job.title} subheader={new Date(props.job.created_at).toLocaleDateString()} />
+			<div className={classes.imgContainer}>
+				<CardMedia className={classes.media} component="img" src={props.job.company_logo} title={props.job.company} />
+			</div>
+			<CardContent>
+				<div className={classes.chipLabels}>
+					<Chip size="small" icon={<LocationIcon />} color="primary" label={props.job.location} />
+					<Chip size="small" icon={<WatchIcon />} color="secondary" label={props.job.type} />
+				</div>
+				<Typography variant="body1">{props.job.company}</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				<Button variant="contained" size="small" color="primary" href={props.job.url} target="_blank" rel="noreferrer">
+					Apply Now
+				</Button>
+				<IconButton
+					className={clsx(classes.expand, {
+						[classes.expandOpen]: expanded,
+					})}
+					onClick={handleExpandClick}
+					aria-expanded={expanded}
+					aria-label="show more">
+					<ExpandMoreIcon />
+				</IconButton>
+			</CardActions>
+			<Collapse in={expanded} timeout="auto" unmountOnExit>
+				<CardContent>
+					<Typography variant="body1">
+						<Markdown>{props.job.description}</Markdown>
+					</Typography>
+				</CardContent>
+			</Collapse>
+		</Card>
 	);
 }
 
