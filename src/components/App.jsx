04d3b9a473d58d117570891475412947a56ca2db
Deeshan Sharma <deeshansharma04@gmail.com>
2021-05-22 23:12:21 +0530

    feat: add dynamic theme & performance improvement

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from "react";
-import { Container, CssBaseline } from "@material-ui/core";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import React, { useState, useMemo, useEffect } from "react";
+import { Container, CssBaseline, useMediaQuery } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import { DarkTheme, LightTheme } from "./Theme";
 import Header from "./Header";
 import Home from "./Home";
 
-function App() {
-	const [darkMode, setDarkMode] = useState(true);
+const useStyles = makeStyles((theme) => ({
+	themeChange: {
+		transition: theme.transitions.create("all", {
+			duration: theme.transitions.duration.shortest,
+		}),
+	},
+}));
 
-	const theme = createMuiTheme({
-		palette: {
-			type: darkMode ? "dark" : "light",
-		},
-	});
+function App() {
+	const classes = useStyles();
+	const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)", { noSsr: true });
+	const [darkMode, setDarkMode] = useState(prefersDarkMode);
+
+	useEffect(() => {
+		setDarkMode(prefersDarkMode);
+	}, [prefersDarkMode]);
+
+	const theme = useMemo(() => createMuiTheme(darkMode ? DarkTheme : LightTheme), [darkMode]);
 
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<Header darkMode={darkMode} setDarkMode={setDarkMode} />
-			<Container>
-				<Home />
-			</Container>
+			<div className={classes.themeChange}>
+				<Header darkMode={darkMode} setDarkMode={setDarkMode} />
+				<Container>
+					<Home />
+				</Container>
+			</div>
 		</ThemeProvider>
 	);
 }
